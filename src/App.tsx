91e99045bad0f46dea369967af229ae8ab29acb7
GuiOSousa <guilherme.olivesousa@gmail.com>
2025-06-03 14:15:22 -0300

    Added Fade Effect on ImageBox

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ArticleModel from "./pages/ArticleModel";
 
+
+
 function App() {
   return (
-    <>
-      <Routes>
+			<Routes>
 				<Route path="/" element={<Home/>} />
         <Route path="/article" element={<ArticleModel/>} />
 			</Routes>
-    </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import ResetScroll from "./functions/ResetScroll.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<BrowserRouter>
+			<ResetScroll/>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
