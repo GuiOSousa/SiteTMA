278e1dad47cf6537e5782ebb7a22877c11089d88
GuiOSousa <guilherme.olivesousa@gmail.com>
2025-07-02 16:01:04 -0300

    Updated Articles Page

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -1,10 +1,21 @@
 import NavigationBar from "../components/NavigationBar"
+import ImageBox from '../components/ImageBox'
+import chatGPT from '../assets/chatGPT.png'
 
 function ArticlePage() {
     return(
         <>
         <NavigationBar/>
         <h1>Artigos</h1>
+        <div className="Space"></div>
+        <div className='ImageBoxDiv'>
+        <ImageBox title="O Impacto Ambiental das IA's" link='/articleAICost' image={chatGPT} />
+        <ImageBox title='Artigo 2' link='/' image='' />
+        <ImageBox title='Artigo 3' link='/' image='' />
+        <ImageBox title='Artigo 4' link='/' image='' />
+        <ImageBox title='Artigo 5' link='/' image='' />
+        <ImageBox title='Artigo 6' link='/' image='' />
+        </div>
         </>
     )
 }
