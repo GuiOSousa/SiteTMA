ac12162af3d549332c5e99a73363eab6a70535fe
Daniel Barbosa Rocha <daniel.dr206@gmail.com>
2025-07-13 19:22:05 -0300

    hotfix

--- src/components/ImageBox.tsx
+++ src/components/ImageBox.tsx
@@ -1,28 +1,31 @@
 import type { FC } from "react";
 import { Link } from "react-router-dom";
-import './ImageBox.css'
-import '../pages/Home.css'
+import "./ImageBox.css";
+import "../pages/Home.css";
 
 interface ImageBoxProps {
-	title: string;
-	link: string;
+  title: string;
+  link: string;
   image: string;
 }
 
-export const ImageBox: FC<ImageBoxProps> = ({title, link, image}) => {
+export const ImageBox: FC<ImageBoxProps> = ({ title, link, image }) => {
   return (
-    <Link to={link} style={{textDecoration: 'none'}}>
-    <div className='ImageBox' style={{
-				backgroundImage: `url(${image})`,
-				backgroundSize: 'cover',
-				backgroundPosition: 'center',
-			}}>
+    <Link to={link} style={{ textDecoration: "none" }}>
+      <div
+        className="ImageBox"
+        style={{
+          backgroundImage: `url(${image})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
         <div className="ImageBoxTextDiv">
-          <div className="IBTitle" >{title}</div>
+          <div className="IBTitle">{title}</div>
         </div>
-    </div>
+      </div>
     </Link>
-  )
-}
+  );
+};
 
-export default ImageBox
+export default ImageBox;
